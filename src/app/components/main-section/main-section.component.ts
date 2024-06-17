0da9f8c4e7995c14d745fd965be7c09eee042e54
DeynerNB <deynernavarrob@gmail.com>
2024-06-16 20:25:21 -0600

    feat(main-section): AutoTyping title

--- src/app/components/main-section/main-section.component.ts
+++ src/app/components/main-section/main-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'component-main-section',
@@ -11,7 +11,7 @@ export class MainSectionComponent {
     full_page_title: string = "Personal Portfolio"
     page_title: string = "P"
 
-    constructor() {
+    ngOnInit() {
         this.setPageTile()
     }
 
