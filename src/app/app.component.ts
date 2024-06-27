533bd6f140b34933604bf8cd4b547db52e7d037d
DeynerNB <deynernavarrob@gmail.com>
2024-06-26 19:32:07 -0600

    refactor(app): change arrange of sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { MainSectionComponent } from './components/main-section/main-section.component';
 import { AboutSectionComponent } from './components/about-section/about-section.component';
@@ -13,6 +13,7 @@ import { PageSliderComponent } from './components/page-slider/page-slider.compon
     templateUrl: './app.component.html',
     styles: '',
     imports: [
+        CommonModule,
         MainSectionComponent,
         AboutSectionComponent,
         ProjectSectionComponent,
@@ -22,4 +23,28 @@ import { PageSliderComponent } from './components/page-slider/page-slider.compon
 })
 export class AppComponent {
     title = 'personal-portfolio';
+
+    @ViewChild('mouse_div')
+    mouse_div!: ElementRef<HTMLDivElement>;
+
+    mouseX: number = 0;
+    mouseY: number = 0;
+
+    offsetW: number = 0;
+    offsetH: number = 0;
+
+    ngAfterViewInit() {
+        if (!this.mouse_div) {
+            return;
+        }
+
+        const rect = this.mouse_div.nativeElement.getBoundingClientRect();
+        this.offsetW = rect.width / 2;
+        this.offsetH = rect.height / 2;
+    }
+
+    onMouseMove(event: MouseEvent) {
+        this.mouseX = event.clientX - this.offsetW;
+        this.mouseY = event.clientY - this.offsetH + window.scrollY;
+    }
 }
